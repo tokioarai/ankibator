0015f552457cd459cc8b3fffcb609b172410085e
新居示雄 <tokioarai@gmail.com>
2017-08-19 16:06:35 +0900

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require "csv"
+
+companies_csv = CSV.readlines("db/companies.csv")
+companies_csv.shift
+companies_csv.each do |row|
+  Company.create(name: row[1], code: row[2], foundation: row[3], industry: row[4], answer1: row[5], answer2: row[6], sales: row[7],profit: row[8])
+end
