5185633970a7ce25a52c6f9b8f6afdcb7f31cf45
新居示雄 <tokioarai@gmail.com>
2017-08-20 19:18:40 +0900

    moromoro

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@ require "csv"
 companies_csv = CSV.readlines("db/companies.csv")
 companies_csv.shift
 companies_csv.each do |row|
-  Company.create(name: row[1], code: row[2], foundation: row[3], industry: row[4], answer1: row[5], answer2: row[6], sales: row[7],profit: row[8])
+  Company.create(code: row[1], name: row[2], industry: row[3], foundation: row[4], sales: row[5], profit: row[6], kessan: row[7],answer1: row[8], answer2: row[9])
 end
